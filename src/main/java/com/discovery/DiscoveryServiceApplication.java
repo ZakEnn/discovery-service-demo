8ebe7270e059522e5cc0740ecedfaae8d17df551
ZakEnn <ennaje.zakari@gmail.com>
2020-09-26 20:40:27 +0100

    refactoring

--- src/main/java/com/discovery/DiscoveryServiceApplication.java
+++ src/main/java/com/discovery/DiscoveryServiceApplication.java
@@ -1,11 +1,13 @@
-package com.easysign;
+package com.discovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
+@RefreshScope
 public class DiscoveryServiceApplication {
 
 	public static void main(String[] args) {
